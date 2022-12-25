93c9ac8aad03d54607259e0ad9d0a2cbe95700a4
Raghunandan-29 <102844162+Raghunandan-29@users.noreply.github.com>
2022-12-25 11:14:16 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,29 @@
+//progress-bars//
+const skillsSection = document.getElementById('skill-section');
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function showProgress(){
+    progressBars.forEach(progressBars=> {
+        const value = progressBars.dataset.progress;
+        progressBars.style.opacity = 1;
+        progressBars.style.width = `${value}%`;
+    });
+}
+function hideProgress(){
+    progressBars.forEach(p=> {
+        p.style.opacity = 0;
+        p.style.width = 0;
+    });
+}
+
+window.addEventListener('scroll',()=>{
+    const sectionPos = skillsSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+
+    if(sectionPos < screenPos){
+        showProgress();
+    }else{
+        hideProgress();
+    }
+
+});
